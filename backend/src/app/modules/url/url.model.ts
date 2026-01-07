818672af82a9459822f7808d2be13f1ca7ab7321
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2026-01-07 21:54:09 +0600

    url validation added

--- backend/src/app/modules/url/url.model.ts
+++ backend/src/app/modules/url/url.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model } from "mongoose";
 import { UrlInterface } from "./url.interface";
 
 const UrlSchema: Schema = new Schema(
